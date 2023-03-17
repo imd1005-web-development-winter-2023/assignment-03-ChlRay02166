e882c8ad8bdc05b5745018dc04493becfda3dd90
ChlRay02166 <124922095+ChlRay02166@users.noreply.github.com>
2023-03-17 11:31:26 -0400

    Update 4 :)!

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+//form where task is inputed
 const form = document.querySelector('#to-do-form');
 const input = document.querySelector('#new-item');
 const ul = document.querySelector('#to-do-list');
 
 form.addEventListener('submit', function(event) {
 	event.preventDefault();
+	//will return text if blank
 	if (input.value === '') {
 		alert('Please enter a task.');
+	//creates the tasks with text/checkbox/button
 	} else {
-		const li = document.createElement('li');
+		const todo = document.createElement('todo');
 		const checkbox = document.createElement('input');
 		const span = document.createElement('span');
 		const button = document.createElement('button');
@@ -15,18 +18,21 @@ form.addEventListener('submit', function(event) {
 		checkbox.type = 'checkbox';
 		span.textContent = input.value;
 		button.textContent = 'Delete';
-
-		li.appendChild(checkbox);
-		li.appendChild(span);
-		li.appendChild(button);
-		ul.appendChild(li);
+        
+		//add at the end of the list
+		todo.appendChild(checkbox);
+		todo.appendChild(span);
+		todo.appendChild(button);
+		ul.appendChild(todo);
 
 		input.value = '';
 
+		//delete button/removes the to do
 		button.addEventListener('click', function() {
-			ul.removeChild(li);
+			ul.removeChild(todo);
 		});
 
+        //if checked, strike through/color change
 		checkbox.addEventListener('change', function() {
 			if (this.checked) {
 				span.style.textDecoration = 'line-through';
